15fc2403a90342c836ac5c1ae7656e7601c3a95c
Shane Richards <shane.richards121@gmail.com>
2015-03-10 19:35:38 -0500

    no change

--- sites/MSBM/app/modules/bookings/BookingsWebModule.php
+++ sites/MSBM/app/modules/bookings/BookingsWebModule.php
@@ -86,22 +86,26 @@ class BookingsWebModule extends WebModule
                 $this->isAccessTokenSet();
 
                 break;
+
             case 'list':
                 $this->isMoodleTokenSet();
 
                 $this->isAccessTokenSet();
 
                 break;
+
             case 'day';
                 $this->isMoodleTokenSet(); // checks if user is logged in
 
                 $this->isAccessTokenSet();
-                $links = array(
-                    array(
+
+                $links = [
+                    [
                         'title' => 'Login',
-                        'url' => $this->buildBreadcrumbURL('login', array())
-                    ),
-                );
+                        'url' => $this->buildBreadcrumbURL('login', [])
+                    ],
+                ];
+
                 $this->assign('links', $links);
                 break;
 
@@ -181,106 +185,3 @@ class BookingsWebModule extends WebModule
 
     }
 }
-
-//
-//    /**
-//     * Initialization Using the API
-//     */
-//    protected function initializeForPage()
-//    {
-//
-//        switch($this->page)
-//        {
-//            case 'index':
-////                print "Please visit:\n$authUrl\n\n";
-//                print "Please enter the auth code:\n";
-//                $authCode = trim(fgets(STDIN));
-//
-//                // Exchange authorization code for access token
-//                $accessToken = $this->client->authenticate($authCode);
-//                $this->client->setAccessToken($accessToken);
-//                break;
-//            case 'create':
-//                if ($token = Kurogo::getCache('token'))
-//                {
-//                    $this->redirectTo('booking/all');
-//                }
-////                $sqlDate = "SELECT * FROM booking booking_time";
-////                $resultDate = $db->query($sqlDate, array('id', 'uwi_id', 'name', 'time_from', 'time_to'));
-////
-////                while ($row = $resultDate->fetch())
-////                {
-////
-////                }
-//
-//                break;
-//            case 'create':
-//
-//                $nav = array(
-//                    'subtitle' => 'Subtitle',
-//                    'title' => 'Booking',
-//                    'url' => $this->buildBreadcrumbURL('book', array())
-//                );
-//                $this->assign('nav', $nav);
-//                $this->assign('message', 'Hello World');
-//
-////                $sqlLocations = "SELECT * FROM booking_location";
-////                $result = $db->query($sqlLocations, array('location', 'field1', 'field2'));
-////                while ($row = $result->fetch())
-////                {
-////                    // add the location data here
-////                }
-//                $form = array(
-//                    array(
-//                        'name' => 'from',
-//                        'title' => 'Time',
-//                        'label' => 'from',
-//                        'subtitle' => '(from)',
-//                        'type' => 'time'
-//                    ),
-//
-//                    array(
-//                        'name' => 'to',
-//                        'title' => 'Time',
-//                        'label' => 'to',
-//                        'subtitle' => '(to)',
-//                        'type' => 'time'
-//                    ),
-//
-//                    array(
-//                        'name' => 'location',
-//                        'title' => 'Location',
-//                        'type' => 'select',
-//                        'default' => 'pagoda1',
-//                        'options' => array(
-//                            'pogoda2',
-//                            'pogoda3',
-//                            'room1'
-//                        ) // fill this inner array from database, gives more ontroll to admin
-//                    ),
-//
-//                    array(
-//                        'name' => 'date',
-//                        'title' => 'Date',
-//                        'type' => 'date',
-//                        'value' => getdate()
-//                    ),
-//
-//                    array(
-//                        'name' => 'submit',
-//                        'value' => 'Submit',
-//                        'type' => 'submit'
-//                    )
-//
-//                );
-//                $this->assign('formFields', $form);
-//                $this->assign('formListID', 'booking-form');
-//                break;
-//            case 'all':
-//                $this->assign('username', $this->getArg('username'));
-//                break;
-//            default:
-//                parent::initializeForPage();
-//                break;
-//        }
-//    }

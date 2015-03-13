8010689160110a6338bf04cd5602c85824ecaaf2
Shane Richards <shane.richards121@gmail.com>
2015-03-12 23:33:52 -0500

    lists events

--- sites/MSBM/app/modules/bookings/BookingsWebModule.php
+++ sites/MSBM/app/modules/bookings/BookingsWebModule.php
@@ -118,6 +118,35 @@ class BookingsWebModule extends WebModule
 
         switch($this->page) {
             case 'index':
+                $this->retrieveAccessToken();
+
+                $events = $this->service
+                    ->events
+                    ->listEvents('41hloqnqe4a9pl0ngpocc2t92g@group.calendar.google.com');
+
+                while (true)
+                {
+                    foreach ($events->getItems() as $event)
+                    {
+                        echo $event->getSummary();
+                    }
+                    $pageToken = $events->getNextPageToken();
+                    if ($pageToken)
+                    {
+                        $optParams = [
+                            'pageToken' => $pageToken
+                        ];
+                        $events = $this->service->events->listEvents('primary', $optParams);
+
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                $this->assign("lol", json_decode($events));
+                var_dump($events);
                 // $this->isMoodleTokenSet(); // checks if user is logged in
 
 //                if (!isset($_COOKIE['moodle_token']))
@@ -253,5 +282,73 @@ class BookingsWebModule extends WebModule
         if (!isset($_COOKIE['moodle_token']))
             $this->redirectTo('login');
 
+    }
+
+    public function retrieveAccessToken()
+    {
+        $conn = mysqli_connect('localhost', 'root', 'root', 'kurogo');
+        if(!$conn){
+            die('Connect Error: ' . mysqli_connect_error());
+        }
+
+
+        $this->client = new Google_Client();
+        // OAuth2 client ID and secret can be found in the Google Developers Console.
+        #refresh token
+        //$refreshToken = '1/bTAaKST4WXP1U16FzobZUqsOkKTK36j2G8dYZKMLjq8';
+        # test values hard coded
+        $this->client->setClientId('987849558796-5a1oa8h6la31s7l8ve5vsisjlhsahfrj.apps.googleusercontent.com');
+        $this->client->setClientSecret('onkohzxipY8Rm-XTeouk8nyV');
+
+        $this->client->setRedirectUri('localhost:8888/bookings/index');
+        $this->client->setRedirectUri('http://kurogo.artuvic.com:8010/courses/index');
+        $this->client->setApplicationName('MSBM');
+
+        $this->client->addScope('https://www.googleapis.com/auth/calendar');
+        $this->client->addScope('shane.richards121@gmail.com');
+
+        $this->service = new Google_Service_Calendar($this->client);
+        //$this->client->authenticate('4/UJTpCcSpvRQn1bEfT2mhfbncIG-5OC3MNtp6caqVzCE.gq-Um0T2p7ESaDn_6y0ZQNhgUaoAmAI');
+        //$this->access_token = $this->client->getAccessToken();
+
+        //$this->client->setAccessToken($refreshToken);
+        //if ($this->client->isAccessTokenExpired())
+        //    $this->client->refreshToken($refreshToken);
+
+        $this->service = new Google_Service_Calendar($this->client);
+        //$this->client->authenticate('4/egQcISXQMD-Tv8PggVgTEb3KVkqf1AkikeEbcBOaNds.cjXCnwZzA4MdaDn_6y0ZQNgmzL4TmAI');
+        //$access_token = $this->client->getAccessToken();
+        //$tokens_decoded = json_decode($access_token);
+        //$refresh_token = $tokens_decoded->refresh_token;
+
+        //$sql = "INSERT INTO google_cal (access_token, refresh_token) VALUES ( '$access_token', '$refresh_token')";
+
+        $get = "SELECT * FROM google_cal";
+        $result = mysqli_query($conn, $get);
+        //if (mysqli_query($conn, $get)) {
+        if (mysqli_num_rows($result) > 0){
+            $tokens = mysqli_fetch_assoc($result);
+            echo "New record created successfully" . $tokens['refresh_token'];
+        } else {
+            echo "Error: " . $get . "<br>" . $conn->error;
+        }
+        $this->access_token = $this->client->setAccessToken($tokens['access_token']);
+        if ($this->client->isAccessTokenExpired()) {
+
+            $this->refresh_token = $tokens['refresh_token'];
+            $this->client->refreshToken($this->refresh_token);
+            $this->access_token = $this->client->getAccessToken();
+            $tokens_decoded = json_decode($this->access_token);
+            $this->refresh_token = $tokens_decoded->refresh_token;
+            echo "Got new access token" . $this->refresh_token;
+            $update = "UPDATE google_cal SET access_token='$this->access_token', refresh_token='$this->refresh_token' WHERE id=0";
+            //$result = mysqli_query($conn, $get);
+            if (mysqli_query($conn, $update)) {
+                echo "Record updated successfully";
+            } else {
+                echo "Error updating record: " . mysqli_error($conn);
+            }
+            $this->client->authenticate($this->access_token);
+        }
     }
 }
